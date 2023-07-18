261022234c1b04a4c73986873532106e2011490b
rmeirosu <rmeirosu@deloittece.com>
2023-07-18 16:52:56 +0300

    Java Intro println modifications

--- Main.java
+++ Main.java
@@ -3,5 +3,18 @@ public class Main {
 			System.out.println("Hello World");
 			System.out.println("I am learning Java");
 			System.out.println("It is awesome!");
+			System.out.println("You can use println to print numbers:");
+			System.out.println(3);
+			System.out.println(358);
+			System.out.println(50000);
+			System.out.println("You can also print math:");
+			System.out.println(3 + 3);
+			System.out.println(2 * 5);
+			System.out.println("This line has a comment after it.");
+			//This is a single-line comment
+			System.out.println("This line has a multi-line comment after it.");
+			/* This is a 
+			multi-line
+			comment*/
 	}
 }
