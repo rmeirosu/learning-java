d6ba981eebfa29dd3fbe6ba1a93cb96dfb1c0f1b
rmeirosu <rmeirosu@deloittece.com>
2023-07-20 14:14:17 +0300

    Finished section 3

--- udemy.java
+++ udemy.java
@@ -8,7 +8,7 @@ public class udemy {
 		//Section 3 - First Steps
 			System.out.println("Hello World");
 			System.out.println("Hello, Razvan");
-		
+/////////////////////////////////////////////////////////////////////////////////		
 		/*Section 3 - First Steps
 		==Variables==
 		-In general variables must be initialised before use.
@@ -29,7 +29,7 @@ public class udemy {
 		myFirstNumber = (10 + 5) + (2 *10); //more math
 			System.out.println("More math: " + myFirstNumber);
 		//Java operators (+, *, etc) perform an operation on a variable value.
-		
+/////////////////////////////////////////////////////////////////////////////////		
 		/*Section 3 - First Steps
 		==Starting out with Expressions==*/
 		int mySecondNumber = 12;
@@ -44,7 +44,7 @@ public class udemy {
 		//challenge
 		int myLastOne = 1000 - myTotal;
 			System.out.println("My Last Math is " + myLastOne);
-		
+/////////////////////////////////////////////////////////////////////////////////		
 		/*Section 3 - First Steps
 		==Primitive Types==
 		-most basic data types
@@ -63,7 +63,7 @@ public class udemy {
 			//minimum value when it underflows wraps around to the maximum value and continues processing
 		/*int myMaxIntTest = 2147483648;
 			-Assigning a value outside the range throws an error.*/
-		
+/////////////////////////////////////////////////////////////////////////////////		
 		/*Section 3 - First Steps
 		==byte, short, long and width==
 		-byte supports the smallest range
@@ -80,7 +80,7 @@ public class udemy {
 		long myLongValue = 100L; //Java default stores as int. We've stored a small value (100) as a long
 			System.out.println("A Long has a width of: " + Long.SIZE);
 		  	System.out.println("Long Value Range: (" + Long.MIN_VALUE + " to " + Long.MAX_VALUE + ")");
-		
+/////////////////////////////////////////////////////////////////////////////////		
 		/*Section 3 - First Steps
 		==Casting in Java==
 		Casting is a way to get Java to treat a variable of one type like a different data type.*/
@@ -93,7 +93,7 @@ public class udemy {
 			-We put the type we want the number to be in parantheses.*/
 		byte myNewByteValue = (byte) (myMinByteValue / 2);
 			System.out.println("myNewByteValue is: " + myNewByteValue);
-			
+/////////////////////////////////////////////////////////////////////////////////			
 		/*Section 3 - First Steps
 		==Primitive Types Challenge==
 		Create four new variables:
@@ -108,7 +108,7 @@ public class udemy {
 		int aIntVar = 3_000_000;
 		long aLongVar = (50_000 + (10 * (aByteVar + aShortVar + aIntVar)));
 			System.out.println("My Long variable is: " + aLongVar);
-
+/////////////////////////////////////////////////////////////////////////////////
 		/*Section 3 - First Steps
 		==Float and Double Primitives==
 		-Floating-point number is used for more precision in calculations.
@@ -121,31 +121,231 @@ public class udemy {
 			System.out.println("Float Range Value (" + Float.MIN_VALUE + " to " + Float.MAX_VALUE + ")");
 			System.out.println("Double Range Value (" + Double.MIN_VALUE + " to " + Double.MAX_VALUE + ")");
 		int myIntValue = 5;
-		float myFloatValue = 5;
-		double myDoubleValue = 5;
-			System.out.println("Values are: myIntValue = " + myIntValue + ", " + "myFloatValue = " + myFloatValue + ", " + "myDoubleValue = " + myDoubleValue);
-		
+		float myFloatValue = 5F;
+		double myDoubleValue = 5D;
+			System.out.println("Values are: myIntValue = " + myIntValue + ", " + "myFloatValue = " 
+				+ myFloatValue + ", " + "myDoubleValue = " + myDoubleValue);
+/////////////////////////////////////////////////////////////////////////////////		
 		/*Section 3 - First-Steps	
-		==Floating Point Precision and a Challenge==
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-				
-			
+		==Floating Point Precision and a Challenge==*/
+		myIntValue = 5 / 2; //both 5 and 2 are int and the result is an int value (2) instead of decimal (2.5)
+			System.out.println("myIntValue is: " + myIntValue);
+		myFloatValue = 5F / 2F; //changing to float we'll see the decimal precision
+			System.out.println("myFloatValue is: " + myFloatValue);
+		myDoubleValue = 5D / 2D;
+			System.out.println("myDoubleValue is: " + myDoubleValue);
+			//Change the divisor to 3
+		myIntValue = 5 / 3;
+			System.out.println("new myIntValue is: " + myIntValue);
+		myFloatValue = 5F / 3F;
+			System.out.println("new myFloatValue is: " + myFloatValue);
+		myDoubleValue = 5D / 3D; //note the change in precision
+			System.out.println("new myDoubleValue is: " + myDoubleValue);
+		myDoubleValue = 5.00 / 3.00; //using decimals instead of sufix (D) and Java will know it's a double type - resulting a double
+			System.out.println("decimal myDoubleValue is: " + myDoubleValue);		
+		myDoubleValue = 5.00 / 3; //using only one operand with decimals and Java will know it's a double type - resulting a double
+			System.out.println("one operand double myDoubleValue is: " + myDoubleValue);
+		/*Challenge: Convert a given number of pounds to kilograms.
+		STEPS:
+			1. Create a var with the appropriate type, to store the number of pounds that we want to convert.
+			2. Calculate kg using the variable above and store the result in a 2nd appropriately typed variable.
+			3. Print the result.
+			4. Conversion formula: 1 pounds is equal to 0.45359237 of a kilogram.*/
+		double storePounds = 200D;
+		double storeKilograms = storePounds * 0.45359237;
+			System.out.println(storePounds + " pounds = " + storeKilograms + " kg.");
+		/*  -In general float and double are great for general floating point operations.
+			-But neither should be used when precise calculations are required.
+			-This is due to a limitation with how floating point numbers are stored.
+			-Java has a class called BigDecimal that overcomes this.*/
+/////////////////////////////////////////////////////////////////////////////////
+		/*Section 3 - First-Steps	
+		==The char and boolean Primitive Data Types==*/
+		char myChar = 'D';
+			System.out.println("Literal representation of char: " + myChar + " ('D')");
+		/*Differences between char and String:
+		char: 
+			-Holds one and only one character.
+			-Literal enclosed in Single Quotes ('').
+		String:
+			-Can hold multiple characters.
+			-Literal enclosed in Double Quotes ("").*/
+		myChar = '\u0044';
+			System.out.println("Unicode representation of char: " + myChar + " (u0044)");		
+		myChar = 68;
+			System.out.println("Integer representation of char: " + myChar + " (68)");
+		/* Char Challenge:
+		Create three char variables to store the character for the question-mark symbol:
+			1.mySimpleChar should be assigned the literal question-mark character: ?.
+			2.myUnicodeChar should be assigned the unicode value for the question-mark character: ?. 
+			3.myDecimalChar should be assigned the decimal value for the question-mark character: ?.
+		Print all three variables in one statement, that starts with the label 'My values are '.*/
+		char mySimpleChar = '?';
+		char myUnicodeChar = '\u003f';
+		char myDecimalChar = 63;
+			System.out.println("My values are " + mySimpleChar + ", " + myUnicodeChar + ", " + myDecimalChar);
+		//Boolean - A boolean value allows for two opposite choices, true or false, yes or no, one or zero.
+		boolean myTrueBooleanValue = true;
+		boolean myFalseBooleanValue = false;
+		boolean isCustomerOverTwentyOne = true; 
+		/*often use the word 'is' as a prefix for a boolean variable name. also 'has', etc.
+		this identifies more clearly as what condition is tested.*/
+/////////////////////////////////////////////////////////////////////////////////
+		/*Section 3 - First-Steps	
+		==Primitive Types Recap and the String Data Type==
+		A String is a class that contains a sequence of characters.*/
+		String myString = "This is a string";
+			System.out.println("myString is equal to: " + myString);
+		myString = myString + ", and this is more.";
+			System.out.println("myString is equal to: " + myString);
+		myString = "I wish I had \u00241,000,000.00";
+			System.out.println("myString is equal to: " + myString);
+			{
+				String lastString = "10";
+				int myInt = 50;
+				lastString = lastString + myInt; 
+				/*even though we've used an integer data type 
+				we'll have the values concatenated ("1050")
+				because lastString variable was configured as String*/
+					System.out.println(lastString);
+				//more examples
+				double doubleNumber	= 120.47;
+				lastString = lastString + doubleNumber;
+					System.out.println(lastString);
+			}
+		/*Strings are immutable:
+		-Immutable means that you can't change a String after it's created.
+		-In our above examples, the value 120.47 is technically not appended to the current contents of lastString.
+		-Instead, a new String is created automatically by Java. The new String consists of the previous value
+		of lastString, plus a textual representation of the double value 120.47.
+		-The old string will be discarded from memory automatically.
+
+		String vs StringBuilder:
+		-The String class is immutable, but can be used much like a primitive data type.
+		-The StringBuilder class is mutable, but does not share the String's special features,
+		such as being able to assign it a String literal or use the + operator on it.*/
+/////////////////////////////////////////////////////////////////////////////////
+		/*Section 3 - First-Steps	
+		==Operators, Operands and Expressions==
+		Operators:
+			-Operators in Java are special symbols that perform specific operations on one, two or 
+			three operands and then return a result.
+		Operands:	
+			-An operrand is a term used to describe any object that is manipulated by an operator.
+		Example:
+			-example: int myVar = 15 + 12; '+' sign is the operator, '15' and '12' are the operands.
+			-Variables used instead of literals (myVar instead of 15) can be used as operands.
+		Expressions:
+			-An expression is formed by combining variables, literals, method return values and 
+			operators.
+			-They are a way of forming and combining those values to produce a result.*/				
+		{
+			int result = 1 + 2; // 1 + 2 = 3
+			int previousResult = result;
+			result = result - 1; // 3 - 1 = 2	
+				System.out.println(result);
+		}
+		{
+			char firstChar ='A';
+			char secondChar = 'B';
+				System.out.println(firstChar + secondChar); //result is 131
+			/*When using + operator with char, it adds together the byte numbers of the char.
+			Character values don't get concatenated.*/
+				System.out.println("" + firstChar + secondChar); //results in AB
+			/*Starting with double quotes ("") means an empty string which is a string literal.
+			Any plus (+) after a string is going to be a concatenation operator.*/
+		}
+		{
+			int result = 2;
+			result = result * 10; //multiplication - 2 * 10 = 20
+				System.out.println(result);
+			result = result / 4; //division - 20 / 4 = 5
+				System.out.println(result);
+		}
+		{
+			/*The Remainder operator
+			-Is represented in Java by the % sign.
+			-It goes by several other names: modulus, modulo or just plan mod for short.
+			-It returns the remaining value from a division operation.
+			-If there is no remaining value the result is 0.*/
+			int result = 5;
+			result = result % 3; // the remainder of (5 % 3) = 2
+				System.out.println(result);
+		}
+/////////////////////////////////////////////////////////////////////////////////
+		/*Section 3 - First-Steps	
+		==Abbreviating Operators==*/
+		{
+			int result = 1;
+			result = result + 1;
+				System.out.println("result by normal math = " + result);
+			result = result - 1;
+				System.out.println("result by normal math = " + result);
+		}		
+		/*Incrementing by One:
+		-Is a very common requirement in programming.
+		-We can do: result = result + 1; But we also have two other shorthand ways to do this same thing:
+			-Post-fix Increment Operator: result++;
+			-Compound Assignment Operator with + sign: result+=1;
+		Decrementing by One:
+		-Also very common.
+		-We can do: result = result - 1; But we also have two other shorthand ways to do this same thing:
+			-Post-fix Decrement Operator: result--;
+			-Compound Assignment Operator with - sign: result-=1;
+		-Unlike Increment and Decrement we can use Compound Assignment to do more than just +1.
+		*/
+		{
+			int result = 1;
+			result++; //postfix increment operator
+				System.out.println("result by postfix increment operator = " + result);
+			result--; //postfix decrement operator
+				System.out.println("result by postfix decrement operator = " + result);
+		}
+		{
+			int result = 1;
+			result += 1; //compound assignment operator with + sign
+				System.out.println("result by compound assignment with plus = " + result);
+			result -= 1; //compound assignment operator with - sign
+				System.out.println("result by compound assignment with minus = " + result);
+		}
+		{
+			int result = 1;
+			result += 5; //compound assignment for more than just +1
+				System.out.println("compound assignment with plus more than just +1 = " + result);
+			result -= 5; //compound assignment for more than just -1
+				System.out.println("compound assignment with minus more than just -1= " + result);
+		}		
+		/*Challenge:
+			1. Initialize an int variable, named result, to the value of 10, rather than 1.
+			2. Use the compound assignment operator with the minus signe to subtract
+			a number from result, using a value of your choice.
+			3. Print the result.*/
+		{
+			int result = 10;
+			result -= 12;
+				System.out.println("increment challenge result = " + result);
+		}
+		{
+			int result = 10;
+			result -= 5.5; //normal math results in 4.5 but due to incompatible data type it prints 4
+			//result = result - 5.5; //note the error thrown for conversion from double to int
+			//what java does is implicitly cast to int when using compound assignment: result = (int) (result - 5.5);
+				System.out.println("challenge result with incompatible data type = " + result);
+		}
+		{
+			double result = 10; //changing the data type to double will ensure correct calculation results
+			result -= 5.5;
+				System.out.println("challenge result with correct data type = " + result);
+		}
+		{
+			double result = 10;
+			result *= 1.5; //multiplication
+				System.out.println("compound assignment division multiplication = " + result);
+		}
+		{
+			double result = 10;
+			result /= 1.5; //division
+				System.out.println("compound assignment division division = " + result);
+		}
 	}
 }
